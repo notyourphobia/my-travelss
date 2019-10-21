0e1a31ea7c6c7dc06b883420ce41890db5d045c4
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-10-21 10:26:22 +0300

    img link

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Travel from './Travel';
 function App() {
   return (
     <div className="madagascar">
-      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/1.jpg")} />
-      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/2.jpg")} />
-      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/3.jpg")} />
-      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/4.jpg")} />
-      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/5.jpg")} />
-      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/6.jpg")} />
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={"https://i.imgur.com/mUtqr1P.jpg"} />
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={"https://i.imgur.com/H5vk9bo.jpg"} />
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={"https://i.imgur.com/rDU3VIl.jpg"} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={"https://i.imgur.com/0h7sS1M.jpg"} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={"https://i.imgur.com/As8VVCE.jpg"} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={"https://i.imgur.com/5VPcEYc.jpg"} />
     </div>
   );
 }
