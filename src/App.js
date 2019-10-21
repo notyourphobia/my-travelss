3a41039ac76f51c6e8cb36a7e4b53ce59fbe4652
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-10-21 09:47:53 +0300

    'idk'

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 import Travel from './Travel';
 
+
 function App() {
-  return ( <React.Fragment>
-  <Travel country="Earth"  destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/_0000_NeonTokyo1.jpg")}/>
-  <Travel country="Earth"  destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/_0001_NeonTokyo2.jpg")}/>
-  <Travel country="Earth"  destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/_0002_NeonTokyo3.jpg")}/>
-  <Travel country="Romania"  destination="Bucharest" distance="0 m" photo={require("./img/_0003_Bucharest1.jpg")}/>
-  <Travel country="Romania"  destination="Bucharest" distance="0 m" photo={require("./img/_0004_Bucharest2.jpg")}/>
-  <Travel country="Romania"  destination="Bucharest" distance="0 m" photo={require("./img/_0005_Bucharest3.jpg")}/></React.Fragment>
+  return (
+    <div className="madagascar">
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/1.jpg")} />
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/2.jpg")} />
+      <Travel country="Earth" destination="Neo-Tokyo" distance="58km^3/week" photo={require("./img/3.jpg")} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/4.jpg")} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/5.jpg")} />
+      <Travel country="Romania" destination="Bucharest" distance="0 m" photo={require("./img/6.jpg")} />
+    </div>
   );
 }
 
